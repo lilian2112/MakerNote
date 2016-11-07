19554d479fd2a8d3207a385161f6b9b0cbf15d6b
RachaelMahon <rachmahon@gmail.com>
2016-11-07 17:32:00 +0000

    Truncation test added

--- spec/noteSpec2.js
+++ spec/noteSpec2.js
@@ -3,10 +3,23 @@ function testNewNote() {
   var notes = new Notes();
   notes.addNote("Butts");
   if
-  (notes.notesArray[0] != "Butts")
+  (notes.notesArray[0] == "Butts")
   {
-    return "Error";
+    return "Passed: testNewNote";
   } else {
-    return "Passed";
+    return "Errorz: testNewNote";
   }
+}
+
+function testTruncation() {
+  var notes = new Notes();
+  notes.addNote("I like big butts and I am unable to be deceitful about it.");
+  notes.truncateNote();
+  if
+    ( notes.abbreviatedArray[0] == "I like big butts and" )
+    {
+      return "Passed: testTruncation";
+    } else {
+      return "Errorz: testTruncation";
+    }
 }
