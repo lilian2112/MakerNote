028048ca787875105a4f9516b9a11207951f3c72
RachaelMahon <rachmahon@gmail.com>
2016-11-08 17:13:55 +0000

    tests refactored to match master branch and test library and suite create

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,9 +1,11 @@
-var notes = new Notes();
-notes.addNote("Butts");
-toEqual(notes.notesArray[0],"Butts");
+window.onload = function()
+{
+var notes = new NoteList();
+   notes.addNote("Butts");
+document.getElementById("demo").innerHTML = toEqual(notes.notes[0],"Butts");
 
-
-var notes = new Notes();
-  notes.addNote("I like big butts and I am unable to be deceitful about it.");
-  notes.truncateNote();
-  toEqual(notes.abbreviatedArray[0], "I like big butts and" );
+ var notes = new NoteList();
+   notes.addNote("I like big butts and I am unable to be deceitful about it.");
+   notes.truncateNote();
+ document.getElementById("demo1").innerHTML = toEqual(notes.truncatedArray[0], "I like big butts and" );
+}
