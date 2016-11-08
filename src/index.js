9ffc154f4173da2d42ba7bd0c3dc2cddfc93725c
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-11-08 15:58:43 +0000

    js implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
-var noteList = new NoteList();
-var saveNote = document.getElementById("addNoteButton");
-var noteText = document.getElementById("noteText").value;
-noteList.addNote(noteText);
-var notes = noteList.truncateNote();
+
+noteList = new NoteList();
+
+window.onload = function()
+{
+  makeUrlChangeShowNote();
+  // function to display notes
+  function displayNotes(){
+    var notes = noteList.notes
+    var result = ""
+    for (var i = 0; i < notes.length; i++) {
+      result += " <li style='list-style: none'><a href='#" + notes[i] + "'>" + notes[i].substring(0,20) + "</a></li>";
+    }
+    document.getElementById("note_list").innerHTML = result
+  };
+
+  document.getElementById("add_note_button").addEventListener('click', function(){
+    event.preventDefault();
+    addNote();
+  });
+
+  //function to add note
+  function addNote(){
+    var noteText = document.getElementById("note_text").value;
+    noteList.addNote(noteText);
+    displayNotes();
+  };
+
+  //function to display full note
+  function makeUrlChangeShowNote() {
+    window.addEventListener("hashchange", showNoteForCurrentPage);
+  };
+
+  function showNoteForCurrentPage() {
+    showNote(getNoteFromUrl(window.location));
+  };
+
+  function getNoteFromUrl(location) {
+    return location.hash.split("#")[1];
+  };
+
+  function showNote(note) {
+    document
+    .getElementById("full_note")
+    .innerHTML = note;
+  };
+}
